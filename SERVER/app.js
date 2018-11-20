aba65d6126b328c3cfe33f973633a560d48eb49b
kwizeraelvis <kwizeraelvis6@gmail.com>
2018-11-20 09:02:29 +0200

    Intial commit

--- SERVER/app.js
+++ SERVER/app.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json());
 
 const packages = [
     { Pid: 1, Powner: 'Elvis', Ppickup: 'Tanzania', Pdestination: 'Rwanda', Pweight: 30, Pprice: 150000 },
-    { Pid: 1, Powner: 'Elvis', Ppickup: 'Tanzania', Pdestination: 'Rwanda', Pweight: 30, Pprice: 150000 },
-    { Pid: 1, Powner: 'Elvis', Ppickup: 'Tanzania', Pdestination: 'Rwanda', Pweight: 30, Pprice: 150000 },
+    { Pid: 2, Powner: 'Elvis', Ppickup: 'Tanzania', Pdestination: 'Rwanda', Pweight: 30, Pprice: 150000 },
+    { Pid: 3, Powner: 'Elvis', Ppickup: 'Tanzania', Pdestination: 'Rwanda', Pweight: 30, Pprice: 150000 },
     { Pid: 1, Powner: 'Elvis', Ppickup: 'Tanzania', Pdestination: 'Rwanda', Pweight: 30, Pprice: 150000 },
     { Pid: 1, Powner: 'Elvis', Ppickup: 'Tanzania', Pdestination: 'Rwanda', Pweight: 30, Pprice: 150000 },
     { Pid: 1, Powner: 'Elvis', Ppickup: 'Tanzania', Pdestination: 'Rwanda', Pweight: 30, Pprice: 150000 },
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 app.get('/api/v1/packages', (req, res) => {
     res.send(packages);
 });
-app.get('api/v1/packages/:Pid', (req, res) => {
+app.get('/api/v1/packages/:Pid', (req, res) => {
     const package = packages.find(c => c.Pid === parseInt(req.params.Pid));
     if (!package) {
         return res.status(404).send("The Package was not found")
@@ -37,7 +37,7 @@ app.get('api/v1/packages/:Pid', (req, res) => {
     }
 });
 
-app.get('api/v1/packages/:Powner', (req, res) => {
+app.get('/api/v1/packages/:Powner', (req, res) => {
     const powner = parcels.find(e => e.Powner === req.params.Powner);
     if (!powner) {
         return res.status(400).send("Person not found");
@@ -47,7 +47,7 @@ app.get('api/v1/packages/:Powner', (req, res) => {
 });
 
 
-app.put('api/v1/packages/:Pid/cancel', (req, res) => {
+app.put('/api/v1/packages/:Pid/cancel', (req, res) => {
     const amendPackages = packages.find(e => e.Pid === parseInt(req.params.Pid));
     if (!amendPackages) {
         return res.status(404).send("The Package was not Found");
@@ -67,8 +67,8 @@ app.post('/api/v1/packages', (req, res) => {
     let Pheight = req.body.Pheight;
     let Plength = req.body.Plength;
     let Pwidth = req.body.Pwidth;
-    let Pvolume = parseFloat(Pheight * Plength * Pwidth);
-    let Pprice = parseFloat(PackageVolume) * 15000;
+    //.let Pvolume = parseFloat(Pheight * Plength * Pwidth);
+    let Pprice = req.body.Pprice;
     const deliveryOrder = {
         Pid,
         Pname,
@@ -81,7 +81,7 @@ app.post('/api/v1/packages', (req, res) => {
         Plength,
         Pprice
     };
-    if (isNaN(Pvolume) || isNaN(Pprice)) {
+    if (isNaN(Pweight) || isNaN(Pprice)) {
         res.send("Check the weight or price for any errors");
     } else {
         packages.push(deliveryOrder);
